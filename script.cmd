60fd283f661cf57196cd70016f2ea44771f8c548
@sqlmaster <globalalek@gmail.com>
2023-12-11 12:47:35 +0300

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -3,13 +3,13 @@ chcp 1251
 set "target_directory=C:\Users\ext_ascheglov\Downloads"
 set "file_script=C:\Users\ext_ascheglov\Downloads\ALL_SQL_SCRIPTS_OUT.sql"
 
-:: перекодировка в UTF8
+:: Перекодировка в UTF8
 set EncodingTo=UTF8
 for /f "delims=" %%f in (
     'dir /b /s "%target_directory%\*.sql" ^| findstr /v /i /e /c:"%file_script%"'
 ) do (
-    for /f "delims=" %%i in ('powershell -Command "(Get-Content '%%f' -Raw) -match '^[^\x00-\x7F]*$'"') do (
-        if "%%i" neq "" (
+    for /f "delims=" %%i in ('powershell -Command "& { Install-Module -Name chardet -Force -Scope CurrentUser; $encoding = (Get-Content -Path '%%f' -Encoding Byte -Raw) | ConvertTo-ChardetEncoding; Write-Output $encoding; }"') do (
+        if "%%i" -eq "UTF-8" (
             (copy "%%f" "%%f-utf8-already") && (
                 echo File is already in UTF-8 format: "%%f"
             ) || (
@@ -21,15 +21,14 @@ for /f "delims=" %%f in (
     )
 )
 
-::pause
-:: удаляем оригиналы
-del "%target_directory%\*.sql"
+:: Пауза
+:: del "%target_directory%\*.sql"
 
-:: переименовываем сконвертированные файлы в файлы с исходными именами
+:: Переименовываем сконвертированные файлы в файлы с исходными именами
 ren "%target_directory%\*.sql-utf8" "*.sql"
 ren "%target_directory%\*.sql-utf8-already" "*.sql"
 
-::pause
+:: Пауза
 :: формируем исполнительный файл
 echo :on error exit > %file_script%
 echo print 'Timestamp start ........................... '+ convert^(varchar^(27^),sysdatetime^(^)^)+' ---^> ['+@@servername+'].'+db_name^(^) >> %file_script%
