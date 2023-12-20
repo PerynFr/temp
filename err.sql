7ad3e3ec404011864fbb41a185557597b38ec502
@sqlmaster <globalalek@gmail.com>
2023-12-20 17:18:23 +0300

    Update err.sql

--- err.sql
+++ err.sql
@@ -2,3 +2,16 @@
 Запрос
 INSERT OPENQUERY(VTB_KK, 'select UF_NAME, UF_FILE, UF_XML_ID from vtb_kk.vtb_product_document') VALUES ('Правила фонда', 5394,'pdu_urk_26')
 Три параметра должны вставить, но в результате первое NULL
+
+
+нужно использовать SELECT с INSERT INTO, а не INSERT с SELECT. Ваш текущий запрос пытается вставить результаты запроса в таблицу, а не вставить значения из запроса.
+
+Вот правильный запрос:
+
+sql
+Скопировать
+
+INSERT INTO [your_table] (UF_NAME, UF_FILE, UF_XML_ID)
+SELECT UF_NAME, UF_FILE, UF_XML_ID 
+FROM OPENQUERY(VTB_KK, 'SELECT UF_NAME, UF_FILE, UF_XML_ID FROM vtb_kk.vtb_product_document')
+WHERE [your_condition]
