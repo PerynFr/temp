03bd5ed004484587fe8ad3d8b9e4ef5c59fc51e3
@sqlmaster <globalalek@gmail.com>
2023-12-28 22:13:06 +0300

    Update tempdb.sql

--- tempdb.sql
+++ tempdb.sql
@@ -1,6 +1,12 @@
-USE [tempdb];
+USE tempdb;
 GO
-EXEC sp_MSforeachtable "IF (OBJECT_ID(N'[dbo].[##temp_##]') IS NOT NULL) DROP TABLE [dbo].[##temp_##];";
+
+-- Удалите все временные таблицы с префиксами #
+EXEC sp_MSForEachTable "IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '?' AND TABLE_TYPE = 'BASE TABLE' AND TABLE_NAME LIKE '#%')
+    DROP TABLE ?";
 GO
-DBCC SHRINKDATABASE (tempdb, 0);
+
+-- Удалите все временные таблицы с префиксами ##
+EXEC sp_MSForEachTable "IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '?' AND TABLE_TYPE = 'BASE TABLE' AND TABLE_NAME LIKE '##%')
+    DROP TABLE ?";
 GO
